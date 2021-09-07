62f8d14e24e356db0ce5806105c6b195b9ef1a9e
krozy <krozy0820@hotmail.com>
2021-09-07 08:22:31 +0800

    - game page completed - drop down menu added

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,6 +3,7 @@ import avatar from 'assets/img/avatar.png'
 import logo from 'assets/img/logo.png'
 import ImgButton from  'components/Button/ImgButton'
 import MenuItem from './menuItem'
+import SubMenu from './subMenu'
 import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
 import { faWallet } from '@fortawesome/free-solid-svg-icons'
 
@@ -18,10 +19,10 @@ const Menu = () => {
                     <img src={logo} alt="logo"/>
                 </div>
                 <div className="flex-row float-right mr-15">
-                    <div className = "flex-row">
+                    <div className = "flex-row menu">
                         <MenuItem text="How to Play" />
                         <MenuItem text="Community" />
-                        <MenuItem text="ArcadeMarket" />
+                        <SubMenu text="ArcadeMarket" />
                     </div>
                     <ImgButton className="btn btn-warning btn-image" onClick={onClickButton} image={faWallet} text="Connect Wallet"/>
                     <ImgButton className="btn btn-danger btn-image" onClick={onClickButton} image={faUserAstronaut} text="Play Game"/>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import ImgButton from  'components/Button/ImgButton'
+
+import { faUserAstronaut } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
 const Home: React.FC = () => {
+    const onClickButton = () => {
+        console.log("asdf")
+    }
+
     return (
-        <div> 
-            asdf
+        <div className="page-content"> 
+            <div className="fit-content">
+                <iframe />
+                <div className="rect rect-1" />
+                <div className="rect rect-2" />
+                <div className="rect rect-3" />
+                <div className="rect rect-4" />
+                <div className="rect rect-5" />
+                <div className="rect rect-6" />
+                <div className="rect rect-7" />
+                <div className="rect rect-8" />
+                <div className="rect rect-9" />
+                <div className="rect rect-10" />
+                <div className="rect rect-11" />
+                <div className="rect rect-12" />
+            </div>
+            <div className="flex-row row">
+                <ImgButton className="btn btn-danger btn-image" onClick={onClickButton} image={faUserAstronaut} text="Buy ArcadeDoges"/>
+                <ImgButton className="btn btn-warning btn-image" onClick={onClickButton} image={faShoppingCart} text="Vend at ArcadeMarket"/>
+            </div>
+            
         </div>
     )
 }
